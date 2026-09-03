7d84c2064f8844210ad051e77ac12c1c09da0431
Briana Valmont <briana.valmont@sonarsource.com>
2026-09-03 14:48:25 -0500

    added addtl comment

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 
 public class Hello {
-	# just adding a comment
+	# just adding an additional comment
 	public static void main(String[] args){
 		for (int i = 0; i < 10; i++) {
 			sayHelloWorld();
