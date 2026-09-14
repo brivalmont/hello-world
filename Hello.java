b82991bf4c37677de3e5e0146dd975055698fc0a
Briana Valmont <briana.valmont@sonarsource.com>
2026-09-14 12:05:49 -0500

    another day, another scan. dev15

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
 	# just adding an additional comment
 	# second comment to kick off scan
+	# third comment and you know why
 	public static void main(String[] args){
 		for (int i = 0; i < 10; i++) {
 			sayHelloWorld();
