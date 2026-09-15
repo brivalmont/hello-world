4028e0fdec88dbf91d9880083b18a85d6adba3a4
Briana Valmont <briana.valmont@sonarsource.com>
2026-09-15 09:43:17 -0500

    another day, another scanny dev4

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	# second comment to kick off scan
 	# third comment and you know why
 	# 4,5 ykwtfgo
+	# 6 bad biddies in a mansion
 	public static void main(String[] args){
 		for (int i = 0; i < 10; i++) {
 			sayHelloWorld();
